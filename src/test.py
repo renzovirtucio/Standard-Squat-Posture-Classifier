433a37723723b6288bee63276dcb9012b6161080
Renzo Virtucio <renzojohnvirtucio@gmail.com>
2022-04-25 21:18:23 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,69 +3,78 @@ import joblib
 import mediapipe as mp
 import utils
 import numpy as np
-from utils import calculate_angle, calculate_distance, rescale_frame
+from utils import ResizeWithAspectRatio, calculate_angle, calculate_distance, rescale_frame, resize_with_aspect_ratio
+import time
 # import custom_drawing_utils as mp_drawing
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-def test_model(clf):
+def test_model(clf, webcamMode):
   squat_classes = ["acc", "ak", "bo", "hs", "kvg", "kvr"]
   for i in squat_classes:
-    # cap = cv2.VideoCapture("D:/Documents/CS 198/Data Collection/Test/"+i+".mp4")
-    cap = cv2.VideoCapture("D:/Documents/CS 198/Data Collection/Dataset/Raw Videos/14/"+i+".mp4")
-    # cap = cv2.VideoCapture("D:/Documents/CS 198/Data Collection/Dataset/Segmented Videos/01/"+i+"/"+i+"_05.mp4")
-    # cap = cv2.VideoCapture("C:/Users/Renzo Virtucio/Downloads/acc-zoomedout.mp4")
-    # cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    frame_num = 0
+    cap = ""
+    if not webcamMode:
+      # cap = cv2.VideoCapture("D:/Documents/CS 198/Data Collection/Test/"+i+".mp4")
+      cap = cv2.VideoCapture("D:/Documents/CS 198/Data Collection/Dataset/Raw Videos/07/"+i+".mp4")
+      # cap = cv2.VideoCapture("D:/Documents/CS 198/Data Collection/Dataset/Segmented Videos/01/"+i+"/"+i+"_05.mp4")
+      # cap = cv2.VideoCapture("C:/Users/Renzo Virtucio/Downloads/acc-zoomedout.mp4")
+    else:
+      cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    
+    prev_frame_time = 0
+    new_frame_time = 0
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
       while cap.isOpened():
         ret, frame = cap.read()
+
+        # frame = ResizeWithAspectRatio(frame, width=800)
+        frame2 = ""
+
+        new_frame_time = time.time()
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+
         if ret == True:
-          # frame = rescale_frame(frame, 50)
+          frame = resize_with_aspect_ratio(frame, width=800)
+          frame2 = np.full((frame.shape[0], 300, frame.shape[2]), 255, dtype=frame.dtype)
           # Recolor image to RGB
-          image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-          image.flags.writeable = False
+          # image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+          image=frame
+          # image = cv2.flip(frame, 1)
+          # image.flags.writeable = False
 
           # Make detection
           results = pose.process(image)
 
           # Recolor back to BGR
-          image.flags.writeable = True
+          # image.flags.writeable = True
           # Render detections
           # mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-          image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+          # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
           input_data = []
           try:
             # Extract landmarks
             landmarks = results.pose_landmarks.landmark
             
-            custom_landmarks = [0, 11, 12, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
-            # for j in custom_landmarks:
-            #   input_data += [landmarks[j].x, landmarks[j].y, landmarks[j].z, landmarks[j].visibility]
-            
             left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
             left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
             left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
             left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            left_heel = [landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].y]
             left_foot_index = [landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y]
 
             right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
             right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
             right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
             right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            right_heel = [landmarks[mp_pose.PoseLandmark.RIGHT_HEEL.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_HEEL.value].y]
             right_foot_index = [landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_FOOT_INDEX.value].y]
 
             left_knee_angle = calculate_angle(left_hip, left_knee, left_ankle)
             left_hip_angle = calculate_angle(left_shoulder, left_hip, left_knee)
             left_ankle_angle = calculate_angle(left_knee, left_ankle, left_foot_index)
-            # left_foot_index_angle = calculate_angle(left_heel, left_foot_index, [left_foot_index[0]-0.1, left_foot_index[1]])
             
             right_knee_angle = calculate_angle(right_hip, right_knee, right_ankle)
             right_hip_angle = calculate_angle(right_shoulder, right_hip, right_knee)
             right_ankle_angle = calculate_angle(right_knee, right_ankle, right_foot_index)
-            # right_foot_index_angle = calculate_angle(right_heel, right_foot_index, [right_foot_index[0]-0.1, right_foot_index[1]])
 
             left_knee_to_right_knee = calculate_distance(left_knee, right_knee)
             left_hip_to_right_hip = calculate_distance(left_hip, right_hip)
@@ -126,68 +135,78 @@ def test_model(clf):
                         left_foot_index_to_left_ankle, right_ankle_to_right_heel, right_heel_to_right_foot_index, 
                         right_foot_index_to_right_ankle, hip_width_to_knee_width_ratio]
 
-            # cv2.putText(image, str(int(left_knee_angle)), 
-            #                         tuple(np.multiply(left_knee, [int(frame.shape[1]), int(frame.shape[0])]).astype(int)), 
-            #                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-            #                               )
-            # cv2.putText(image, str(int(right_knee_angle)), 
-            #               tuple(np.multiply(right_knee, [int(frame.shape[1]), int(frame.shape[0])]).astype(int)), 
-            #               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-            #                     )
-            # cv2.putText(image, str(int(left_hip_angle)), 
-            #                        tuple(np.multiply(left_hip, [int(frame.shape[1]), int(frame.shape[0])]).astype(int)), 
-            #                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-            #                             )
-            # cv2.putText(image, str(int(right_hip_angle)), 
-            #                 tuple(np.multiply(right_hip, [int(frame.shape[1]), int(frame.shape[0])]).astype(int)), 
-            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-            #                             )
-            # cv2.putText(image, str(int(left_ankle_angle)), 
-            #                 tuple(np.multiply(left_ankle, [int(frame.shape[1]), int(frame.shape[0])]).astype(int)), 
-            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-            #                             )
-            # cv2.putText(image, str(int(right_ankle_angle)), 
-            #                 tuple(np.multiply(right_ankle, [int(frame.shape[1]), int(frame.shape[0])]).astype(int)), 
-            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
-            #                             )
           except:
             pass
-        
-          # if frame_num == 0: print(input_data)
+
+          textsize = cv2.getTextSize("REPORT", cv2.FONT_HERSHEY_SIMPLEX, 0.75, 1)[0]
+          centerx = (frame2.shape[1] - textsize[0]) // 2
+          cv2.putText(frame2, "REPORT", (centerx, 30), 
+                      cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,0), 1, cv2.LINE_AA)
+          cv2.putText(frame2, 'YOUR SQUAT POSTURE:', (0,62), 
+                      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+          cv2.putText(frame2, 'FEEDBACK:', (0,132), 
+                      cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
 
           if len(input_data) > 0:
             feedback = clf.predict([input_data])
+
             if (feedback != "others"):
-              cv2.rectangle(image, (0,0), (225,75), (255,255,255), -1)
+              posture = {
+                "acc": "Acceptable",
+                "ak": "Anterior Knees",
+                "bo": "Bent Over",
+                "hs": "Half Squat",
+                "kvg": "Knee Valgus",
+                "kvr": "Knee Varus"
+              }
 
-              pred_color = (0,177,64) if str(feedback[0]) == "acc" else (43,75,238)
+              comment = {
+                "Acceptable": "Great job!",
+                "Anterior Knees": "Your knees have\nmoved forward\ntoo much.",
+                "Bent Over": "Try to keep your\nback straight.",
+                "Half Squat": "Go lower!",
+                "Knee Valgus": "Your knees are\npointing inside.",
+                "Knee Varus": "Your knees are\npointing outside."
+              }
 
-              cv2.putText(image, 'PREDICTED SQUAT POSTURE:', (0,12), 
-                          cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-              cv2.putText(image, str(feedback[0]), 
-                          (0,60), 
-                          cv2.FONT_HERSHEY_SIMPLEX, 2, pred_color, 2, cv2.LINE_AA)
-          
-          # Save frame to .jpg
-          # cv2.imwrite('D:/Documents/CS 198/Data Collection/Dataset/Extracted Frames/Frame'+str(frame_num)+'.jpg', image)
-          frame_num += 1
-          
-          cv2.imshow(i+".mp4", image)
-          key = cv2.waitKey(20)
-          if key == ord('q'):
+              feedback = posture[str(feedback[0])]
+
+              pred_color = (0,177,64) if feedback == "Acceptable" else (43,75,238)
+
+              
+              cv2.putText(frame2, feedback, 
+                          (0,100), 
+                          cv2.FONT_HERSHEY_SIMPLEX, 1.25, pred_color, 2, cv2.LINE_AA)
+
+              y0, dy = 160, 25
+              for j, line in enumerate(comment[feedback].split('\n')):
+                  y = y0 + j*dy
+                  cv2.putText(frame2, line, (0, y), cv2.FONT_HERSHEY_SIMPLEX, 0.9, pred_color, 2, cv2.LINE_AA)
+
+          if webcamMode: 
+            image = cv2.flip(image, 1)
+          else:
+            cv2.putText(image, "FPS: " + str(int(fps)), (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 255, 0), 2, cv2.LINE_AA)
+          image = np.hstack((frame2, image))
+          window_name = "Webcam Stream" if webcamMode else i+".mp4"
+          cv2.imshow(window_name, image)
+          key = cv2.waitKey(1)
+          if key == ord('q') or cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
             break
         else:
           break
 
     cap.release()
     cv2.destroyAllWindows()
+    if webcamMode:
+      break
 
   return
 
 def main():
   clf_filename = "../assets/svm_clf.sav"
   clf = joblib.load(clf_filename)
-  test_model(clf)
+  test_model(clf, int(input("Webcam? ")))
   return
 
 if __name__ == "__main__":
