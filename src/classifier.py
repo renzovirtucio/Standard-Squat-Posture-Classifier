5da8f9e671c9753c955bdcc6f183e92dd1c2e853
Renzo Virtucio <renzojohnvirtucio@gmail.com>
2022-04-25 21:19:04 +0800

    Switch classifier to Logistic Regression

--- src/classifier.py
+++ src/classifier.py
@@ -9,6 +9,9 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, classification_report
 import matplotlib.pyplot as plt
@@ -20,16 +23,28 @@ CUSTOM_POSE_CONNECTIONS = frozenset([(11, 12), (11, 23), (12, 24), (23, 24), (23
 
 def split_data():
   ## Preprocess data
-  df = preprocessing.preprocess_data([os.path.normpath(i) for i in 
-          glob.glob('D:\Documents\CS 198\Data Collection\Dataset/New Extracted Data/**/*.csv', 
-          recursive = True)])
+  df_train = preprocessing.preprocess_data([os.path.normpath(i) for i in 
+                        glob.glob('D:\Documents\CS 198\Data Collection\Dataset/New Extracted Data/**/*-train.csv', 
+                        recursive = True)])
+  print("(rows, columns) =", df_train.shape)
+  print(df_train['target'].value_counts())
+
+  df_test = preprocessing.preprocess_data([os.path.normpath(i) for i in 
+                        glob.glob('D:\Documents\CS 198\Data Collection\Dataset/New Extracted Data/**/*-test.csv', 
+                        recursive = True)])
+  print("(rows, columns) =", df_test.shape)
+  print(df_test['target'].value_counts())
+
+  df = pd.concat([df_train, df_test], ignore_index=True)
 
   # Pop target column from dataframe
   y = df['target']
   X = df.drop(['target'], axis=1)
 
   # Split the data for testing and training
-  X_train, X_test, y_train, y_test = train_test_split(X.values, y, test_size=0.40, random_state=0)
+  X_train, X_test, y_train, y_test = train_test_split(X.values, y, test_size=df_test.shape[0], random_state=0, shuffle=False)
+
+  # display(X_test)
 
   return X_train, X_test, y_train, y_test
 
@@ -56,8 +71,8 @@ def show_performance(clf, X_test, y_test):
 
 def svm_clf(X_train, y_train):
   # Fit a support vector machine
-  clf = make_pipeline(StandardScaler(), SVC(random_state=0, verbose=True))
-  print(clf['svc'].get_params())
+  clf = make_pipeline(StandardScaler(), LogisticRegression(random_state=0, max_iter=1000, solver='liblinear'))
+  print(clf['logisticregression'].get_params())
   clf.fit(X_train, y_train)
 
   return clf
